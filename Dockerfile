adc5f3ad4b1eb8bba9916d459c806e24bb810a12
sq0032 <sq0032@gmail.com>
2016-05-01 23:54:00 -0400

    add RDS

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:14.04
 
-RUN apt-get update && apt-get install -y apache2 libapache2-mod-wsgi python python-pip python2.7-dev
+RUN apt-get update && apt-get install -y apache2 libapache2-mod-wsgi python python-pip python2.7-dev libmysqlclient-dev
 COPY apache /etc/apache2/sites-available/000-default.conf
 #RUN service apache2 restart
 
--- MyDjangoSite/settings.py
+++ MyDjangoSite/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'MyDjangoSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydjangositedb',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'mydjangositedb.cvmigsiw1alx.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
+    }             
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 }
 
 
