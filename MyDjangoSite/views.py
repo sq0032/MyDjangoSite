c19e20b3763d50f58cdab3a4cbc173beff71d90b
sq0032 <sq0032@gmail.com>
2016-05-02 09:57:47 -0400

    Add RDS enviroment checker

--- MyDjangoSite/views.py
+++ MyDjangoSite/views.py
@@ -2,7 +2,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 
-
+import json
+import os
 
 def main(request):
-  return JsonResponse({'foo': 'bar'})
+    
+    if 'RDS_HOSTNAME' in os.environ:
+        env = 'AWS EB'
+    else:
+        env = 'LOCAL'
+
+    return JsonResponse({'env':env})
